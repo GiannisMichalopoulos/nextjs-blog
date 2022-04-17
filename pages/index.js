36004eb49484176e63d9e6226cc1dbc7278ecb6b
Giannism <giannis_michalopoulos@hotmail.com>
2022-04-17 22:24:40 +0300

    First Nextjs test app commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="container">
       <Head>
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>This page!</a>
+          </Link>
         </h1>
 
         <p className="description">
@@ -54,8 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +208,6 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
+export default Home;
